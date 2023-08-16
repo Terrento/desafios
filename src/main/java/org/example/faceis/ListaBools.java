04c948a2b8fafca182e1a48fa7697120161b8c7d
Fabrício <fabricioferreira900@gmail.com>
2023-08-16 16:54:33 -0300

    Ajustes e segundo desafio

--- src/main/java/org/example/faceis/ListaBools.java
+++ src/main/java/org/example/faceis/ListaBools.java
@@ -1,10 +1,9 @@
 package org.example.faceis;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class ListaBools {
-    static int verTrues(ArrayList<Boolean> parametroLista){
+    public int verTrues(ArrayList<Boolean> parametroLista){
         int qtd = 0;
         for (Boolean item : parametroLista) {
             if (item == true){
@@ -12,13 +11,6 @@ public class ListaBools {
             }
         }
         return qtd;
-    }
-
-    public static void main(String[] args) {
-        ArrayList<Boolean> argumentoLista = new ArrayList<>();
-        Collections.addAll(argumentoLista, true, false, false, true, true);
-
-        System.out.println("A quantidade de trues é: "+verTrues(argumentoLista));
     }
 }
 
--- src/main/java/org/example/faceis/QtdPalavras.java
+++ src/main/java/org/example/faceis/QtdPalavras.java
@@ -1,5 +1,13 @@
 package org.example.faceis;
 
 public class QtdPalavras {
-
+    public int contarPalavras(String palavras){
+        int total = 1;
+        for (char c: palavras.toCharArray()) {
+            if (Character.isWhitespace(c)){
+                total ++;
+                }
+            }
+        return total;
+        }
 }
