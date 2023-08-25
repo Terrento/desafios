2391fea7e21737ed97ecf49d54aa0793103d1565
Fabrício <fabricioferreira900@gmail.com>
2023-08-25 17:54:49 -0300

    Adaptacoes para outro exercicio

--- src/main/java/org/example/faceis/Calculadora.java
+++ src/main/java/org/example/faceis/Calculadora.java
@@ -1,5 +1,8 @@
 package org.example.faceis;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class Calculadora {
     public double operacaoCalc(double num1, char simb, double num2) throws Exception {
         double resultado = 0;
@@ -26,5 +29,15 @@ public class Calculadora {
         } else{
             throw new Exception("Símbolo inválido");
         }
+    }
+
+    public double operacaoDesconto(double valor, double porcentagem){
+        double resultado = valor-(valor*porcentagem/100);
+
+        DecimalFormat df = new DecimalFormat("#.##");
+        df.setRoundingMode(RoundingMode.CEILING);
+        df.format(resultado);
+
+        return resultado;
     }
 }
---
+++ src/main/java/org/example/faceis/TrocarNome.java
@@ -0,0 +1,2 @@
+package org.example.faceis;public class TrocarNome {
+}
