d3cf21d10d31a5b0f72a912e739722f7272e69c3
Fabrício <fabricioferreira900@gmail.com>
2023-08-29 23:16:35 -0300

    Mais exercicios

--- src/main/java/org/example/medios/ParxImpar.java
+++ src/main/java/org/example/medios/ParxImpar.java
@@ -1,2 +1,20 @@
-package org.example.medios;public class ParxImpar {
+package org.example.medios;
+
+import java.util.ArrayList;
+
+public class ParxImpar {
+    public int gerarNumero(int[] lista){
+        int resultado = 0;
+        int pares = 0;
+        int impares = 0;
+        for (int i = 0; i < lista.length; i++) {
+            if(lista[i] % 2 == 0){
+                pares += lista[i];
+            } else{
+                impares += lista[i];
+            }
+            resultado = (pares > impares) ? pares-impares:impares-pares;
+        }
+        return resultado;
+    }
 }
--- src/main/java/org/example/medios/TrianguloLados.java
+++ src/main/java/org/example/medios/TrianguloLados.java
@@ -1,2 +1,29 @@
-package org.example.medios;public class TrianguloLados {
+package org.example.medios;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
+import java.lang.Math;
+import java.util.Collections;
+import java.util.Locale;
+
+public class TrianguloLados {
+
+    public ArrayList<Double> gerarLista(double numero1){
+        ArrayList<Double> lista = new ArrayList<>();
+
+        double numero2 = numero1*2;
+        double numero3 = numero1*Math.sqrt(3);
+
+        DecimalFormat df = new DecimalFormat("#.##", DecimalFormatSymbols.getInstance(Locale.US));
+        df.setRoundingMode(RoundingMode.CEILING);
+        String temp = df.format(numero3);
+
+        numero3 = Double.parseDouble(temp);
+
+        Collections.addAll(lista, numero2, numero3);
+        return lista;
+    }
+
 }
