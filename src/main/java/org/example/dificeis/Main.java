97699a79fd574059a618988fb14fcb92d760fd95
Fabrício <fabricioferreira900@gmail.com>
2023-09-01 16:49:34 -0300

    Dificeis comecando

--- src/main/java/org/example/dificeis/Main.java
+++ src/main/java/org/example/dificeis/Main.java
@@ -1,2 +1,10 @@
-package org.example.dificeis;public class Main {
+package org.example.dificeis;
+
+public class Main {
+    public static void main(String[] args) {
+        SeteBoom seteBoom = new SeteBoom();
+        int[] ar = {90, 68, 1007};
+
+        seteBoom.acharNumero(ar, '7');
+    }
 }
--- src/main/java/org/example/dificeis/SeteBoom.java
+++ src/main/java/org/example/dificeis/SeteBoom.java
@@ -1,2 +1,23 @@
-package org.example.dificeis;public class SeteBoom {
+package org.example.dificeis;
+
+import java.util.ArrayList;
+
+public class SeteBoom {
+    public void acharNumero(int[] ar, char alvo){
+        boolean validacao = false;
+        for (int i = 0; i < ar.length; i++) {
+            String sAr = Integer.toString(ar[i]);
+
+            for (char j: sAr.toCharArray()) {
+                if (j == alvo){
+                    validacao = true;
+                }
+            }
+        }
+        if (validacao){
+            System.out.println("Boom!");
+        } else {
+            System.out.println("Não há nenhum sete na lista.");
+        }
+    }
 }
