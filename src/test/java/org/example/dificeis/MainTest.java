797048cf14ce1310a9fdc065853194f54f5b4141
Fabrício <fabricioferreira900@gmail.com>
2023-09-03 01:10:17 -0300

    O teste automatizado

--- src/test/java/org/example/dificeis/MainTest.java
+++ src/test/java/org/example/dificeis/MainTest.java
@@ -1,4 +1,15 @@
+package org.example.dificeis;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
+
+    @Test
+    void testeBoom() {
+        SeteBoom seteBoom = new SeteBoom();
+        int[] ar = {90, 68, 1007};
+        assertEquals("Boom!", seteBoom.acharNumero(ar, '7'));
+    }
 }
